4716ee4c552414ca8a483f66f35dfd4ddaca360d
nowas89 <nowas89@gmail.com>
2018-02-05 19:48:05 +0100

    added load button witch loading api data

--- src/App.js
+++ src/App.js
@@ -111,6 +111,9 @@ class App extends Component {
   		componentDidMount() {
 	let parsed = queryString.parse(window.location.search)
 	let acessToken = parsed.access_token
+
+	if(!acessToken) return;
+	
 	fetch('https://api.spotify.com/v1/me', {
 		headers: {'Authorization': 'Bearer ' + acessToken}
 		}).then(response => response.json()).then(data => this.setState({user:
